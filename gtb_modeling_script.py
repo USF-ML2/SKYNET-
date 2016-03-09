9cb584e99159eb1718648f3461c66d5571fb37e6
mickeykedia <mickeykedia@gmail.com>
2016-03-09 13:49:12 -0800

    One driver CV improved

--- gtb_modeling_script.py
+++ gtb_modeling_script.py
@@ -11,55 +11,22 @@ from pyspark.mllib.linalg import Vectors
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import GBTClassifier, RandomForestClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
-from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClassificationEvaluator
-
-
-#logFile = "/Users/mayankkedia/Downloads/spark-1.6.0-bin-hadoop2.6/README.md"
-sc = SparkContext(appName="GBT MODEL", pyFiles = ['/home/hadoop/SKYNET-/features_m.py','/home/hadoop/SKYNET-/sampling_improved.py' ])
-#sc = SparkContext(appName="GBT MODEL")
-AWS_ACCESS_KEY='AKIAIXZCIKL5ZHV3TXBQ'
-AWS_SECRET_ACCESS_KEY = '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'
-sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", AWS_ACCESS_KEY)
-sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", AWS_SECRET_ACCESS_KEY)
-
-#path = '/Users/mayankkedia/code/kaggle/axa_telematics/jsonsNEW/'
-path = 's3://aml-spark-training/drivers.json/'
-#drivers = ['1', '2', '3', '11', '12', '13', '14', '286', '1060', '1280']
-
-driver_sample = [int(s.all_drivers[i].partition(".")[0]) for i in random.sample(xrange(len(s.all_drivers)), 100)]
-tree_num_range = range(5, 105, 5)
-
-versions = [{"version": 1.0, "smoothed": False, "percentiles": False},
-            {"version": 2.0, "smoothed": False, "percentiles": True},
-            {"version": 3.0, "smoothed": True, "percentiles": False},
-            {"version": 4.0, "smoothed": True, "percentiles": True}]
-
-errors = []
-
-FEATURE_VERSION = 1.0
-
-
-
-def filter_false_positives(label, prediction):
-    if label == 0.0 and prediction == 1.0:
-        return True
-    else:
-        return False
-
-
-def filter_false_negatives(label, prediction):
-    if label == 1.0 and prediction == 0.0:
-        return True
-    else:
-        return False
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 
 
 def create_rows_for_rdd(x):
+    """
+
+    :param x:
+    :return:
+    """
     features = list(x[1])
     l = len(features) - 1
     label = float(features.pop(l))
     meta_data = x[0]
-    return Row(label=label, features=Vectors.dense(features), meta_data=Vectors.dense(meta_data))
+    return Row(label=label,
+               features=Vectors.dense(features),
+               meta_data=Vectors.dense(meta_data))
 
 
 def create_feature_rdd(driver, path, sc, version):
@@ -72,100 +39,158 @@ def create_feature_rdd(driver, path, sc, version):
     """
     driver_RDD = s.labelRDDs(driver, path, sc)
     feature_RDD = fb.step_level_features(fb.get_polars(driver_RDD), version["smoothed"])
-    #print "Printing Step Level Feature Return"
-    #print feature_RDD.take(1)
     trip_features = fb.trip_level_features(feature_RDD, version["percentiles"])
-    #print "Printing Trip Level Feature Return"
-
-    #print trip_features.take(1)
     sqlContext = SQLContext(sc)
 
     total_data = sqlContext.createDataFrame(trip_features.map(create_rows_for_rdd))
     return total_data
 
 
-def calculate_accuracy_metrics(predictions, driver, version, num_tree):
+def calculate_accuracy_metrics(predictions):
 
-    metrics = {}
+    """
+    Calculates accuracy metrics for a Prediction DataFrame
 
-    metrics["driver"] = driver
-
-    #predictions.select("prediction", "indexedLabel", "features", "meta_data").show(20)
-
+    :param predictions:
+    :return:
+    """
     evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel",
                                                   predictionCol="prediction")
     accuracy = round(evaluator.evaluate(predictions, {evaluator.metricName: "precision"}), 2)
     recall = round(evaluator.evaluate(predictions, {evaluator.metricName: "recall"}), 2)
-
-
-    print "Test Error for driver {} is {} for version {}".format(driver, 1.0 - accuracy, version["version"])
-
-    metrics["error_rate"] = 1.0-accuracy
-    metrics["recall"] = recall
 
     positive_cases = predictions.filter(predictions["indexedLabel"] == 1.0)
     negative_cases = predictions.filter(predictions["indexedLabel"] == 0.0)
     false_positive_cases = negative_cases.filter(positive_cases["prediction"] == 1.0)
     false_negative_cases = positive_cases.filter(positive_cases["prediction"] == 0.0)
 
+    return [accuracy,
+            recall,
+            positive_cases.count(),
+            negative_cases.count(),
+            false_positive_cases.count(),
+            false_negative_cases.count()]
+
+
+def create_metric_dictionary(test_predictions, training_predictions, driver, version, num_tree):
+    """
+    Creates a dict with important metrics for this round of modeling
+    :param test_predictions:
+    :param training_predictions:
+    :param driver:
+    """
+
+    metrics = {}
+    metrics["driver"] = driver
     metrics["version"] = version["version"]
     metrics["num_trees"] = num_tree
-    metrics["pos_cases"] = positive_cases.count()
-    metrics["neg_cases"] = negative_cases.count()
-    metrics["fp_cases"] = false_positive_cases.count()
-    metrics["fn_cases"] = false_negative_cases.count()
 
-    gbtModel = model.stages[2]
-    print(gbtModel)  # summary only
+    pred_stats = calculate_accuracy_metrics(test_predictions)
+    for k in TEST_METRIC_FIELDS.keys():
+        metrics[k] = pred_stats[TEST_METRIC_FIELDS[k]]
+
+    train_pred_stats = calculate_accuracy_metrics(training_predictions)
+    for k in TRAIN_METRIC_FIELD.keys():
+        metrics[k] = train_pred_stats[TRAIN_METRIC_FIELD[k]]
 
     return metrics
 
-version = versions[3]
-#for version in versions:
-for num_tree in tree_num_range:
-    for driver in driver_sample:
+########################## ACTUAL SCRIPT STARTS HERE #########################
 
-        # Importing Data
+versions = [{"version": 1.0, "smoothed": False, "percentiles": False},
+            {"version": 2.0, "smoothed": False, "percentiles": True},
+            {"version": 3.0, "smoothed": True, "percentiles": False},
+            {"version": 4.0, "smoothed": True, "percentiles": True}]
 
-        total_data = create_feature_rdd(driver, path, sc, version)
+errors = []
 
-        labelIndexer = StringIndexer(inputCol="label",
-                                     outputCol="indexedLabel").fit(total_data)
+TEST_METRIC_FIELDS = {"test_precision": 0,
+                      "test_recall": 1,
+                      "test_positive_count": 2,
+                      "test_negative_count": 3,
+                      "test_fp_count": 4,
+                      "test_fn_count": 5,
+                      }
+TRAIN_METRIC_FIELD = {"train_precision": 0,
+                      "train_recall": 1,
+                      "train_positive_count": 2,
+                      "train_negative_count": 3,
+                      "train_fp_count": 4,
+                      "train_fn_count": 5}
 
-        featureIndexer = VectorIndexer(inputCol="features",
-                                       outputCol="indexedFeatures",
-                                       maxCategories=4).fit(total_data)
+METRIC_FIELDS = ["driver", "num_trees", "version"]
 
-        (trainingData, testData) = total_data.randomSplit([0.7, 0.3])
+CSV_FIELDNAMES = METRIC_FIELDS + TEST_METRIC_FIELDS.keys() + TRAIN_METRIC_FIELD.keys()
 
-        gbt = GBTClassifier(labelCol="indexedLabel",
-                            featuresCol="indexedFeatures",
-                            maxIter=num_tree)
+FILENAME = "single_driver_hyperparameter_selection.csv"
+#FEATURES_FILE = 'features_m.py'
 
-        pipeline = Pipeline(stages=[labelIndexer, featureIndexer, gbt])
+#sc = SparkContext(appName="GBT MODEL",
+#                  pyFiles=['/home/hadoop/SKYNET-/features_m.py','/home/hadoop/SKYNET-/sampling_improved.py' ])
 
-        model = pipeline.fit(trainingData)
+sc = SparkContext(appName="GBT MODEL")
 
-        predictions = model.transform(testData)
+AWS_ACCESS_KEY='AKIAIXZCIKL5ZHV3TXBQ'
+AWS_SECRET_ACCESS_KEY = '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'
+#sc._jsc.hadoopConfiguration().set("fs.s3n.awsAccessKeyId", AWS_ACCESS_KEY)
+#sc._jsc.hadoopConfiguration().set("fs.s3n.awsSecretAccessKey", AWS_SECRET_ACCESS_KEY)
 
-        errors.append(calculate_accuracy_metrics(predictions, driver, version, num_tree))
+path = '/Users/mayankkedia/code/kaggle/axa_telematics/jsonsNEW/'
+#path = 's3://aml-spark-training/drivers.json/'
 
+driver_sample = [int(s.all_drivers[i].partition(".")[0]) for i in random.sample(xrange(len(s.all_drivers)), 1)]
+tree_num_range = range(5, 10, 5)
 
-with open('feature_selection.csv', 'a') as fp:
-    writer = csv.DictWriter(fp,
-                            fieldnames=["error_rate",
-                                        "driver",
-                                        "version",
-                                        "num_trees",
-                                        "recall",
-                                        "pos_cases",
-                                        "neg_cases",
-                                        "fp_cases",
-                                        "fn_cases"],
-                            delimiter=",")
+# Write Header for CSV File which records this information
+with open(FILENAME, 'a') as fp:
+    writer = csv.DictWriter(fp, fieldnames=CSV_FIELDNAMES, delimiter=",")
     writer.writeheader()
-    for e in errors:
-        writer.writerow(e)
+
+
+#version = versions[3]
+for version in versions:
+    for num_tree in tree_num_range:
+        for driver in driver_sample:
+
+            # Importing Data
+
+            total_data = create_feature_rdd(driver, path, sc, version)
+
+            labelIndexer = StringIndexer(inputCol="label",
+                                         outputCol="indexedLabel").fit(total_data)
+
+            featureIndexer = VectorIndexer(inputCol="features",
+                                           outputCol="indexedFeatures",
+                                           maxCategories=4).fit(total_data)
+
+
+            # Splitting Data into TEST/TRAIN
+
+            (trainingData, testData) = total_data.randomSplit([0.7, 0.3])
+
+            # Modeling using GBT Classifier
+            gbt = GBTClassifier(labelCol="indexedLabel",
+                                featuresCol="indexedFeatures",
+                                maxIter=num_tree)
+
+            pipeline = Pipeline(stages=[labelIndexer, featureIndexer, gbt])
+
+            model = pipeline.fit(trainingData)
+
+            tr_p = model.transform(trainingData)
+            te_p = model.transform(testData)
+
+            accuracy_metrics = create_metric_dictionary(test_predictions=te_p,
+                                                        training_predictions=tr_p,
+                                                        driver=driver,
+                                                        version=version,
+                                                        num_tree=num_tree)
+            errors.append(accuracy_metrics)
+
+            # Writing current model information to file
+            with open(FILENAME, 'a') as fp:
+                writer = csv.DictWriter(fp, fieldnames=CSV_FIELDNAMES, delimiter=",")
+                writer.writerow(accuracy_metrics)
 
 
 
